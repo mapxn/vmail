b7eaf7a3663c341b8cb231aeda658cc42fd87d88
mapxn <16299874+mapxn@users.noreply.github.com>
2024-05-11 15:24:17 +0800

    Update Header.tsx

--- apps/remix/app/components/Header.tsx
+++ apps/remix/app/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
       <Link to="/" className="font-bold flex items-center justify-center gap-3">
         <VmailLogo />
         <button className="cool-btn">
-          <span>VMAIL.DEV</span>
+          <span>TM.TXWD.EU.ORG</span>
         </button>
       </Link>
       <Link
@@ -27,15 +27,6 @@ export default function Header() {
         to="/terms"
         className="ml-3 md:ml-8 text-sm md:text-base hidden md:block">
         {t("Terms")}
-      </Link>
-      <Link
-        className="ml-3 md:ml-8"
-        target="_blank"
-        to="https://github.com/oiov/vmail">
-        <button className="whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-background hover:bg-accent hover:text-accent-foreground text-md flex h-[32px] w-[85px] cursor-pointer items-center justify-center rounded-md border-2 p-2 font-semibold hover:opacity-50">
-          <GithubPlat />
-          <div className="ml-1.5 text-sm">Star</div>
-        </button>
       </Link>
     </div>
   );
